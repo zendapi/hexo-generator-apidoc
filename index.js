86cf3ca2678498698a9abb5d43fc4fccdd122163
softboy <zzu_softboy@163.com>
2017-06-29 10:43:49 +0800

    index文件解析完成

--- index.js
+++ index.js
@@ -6,3 +6,31 @@
  * @copyright Copyright (c) 2016-2017 QCoreTeam (http://www.qcoreteam.org)
  * @license   http://www.topjs.org/license/new-bsd New BSD License
  */
+let DsBuilder = require("./lib/ds_builder");
+let Models = require("./lib/models");
+
+hexo.apigen = {};
+
+function register_models(hexo)
+{
+   let db = hexo.database;
+   let keys = Object.keys(Models);
+   let key = '';
+   for (let i = 0, len = keys.length; i < len; i++) {
+      key = keys[i];
+      db.model(key, Models[key](hexo));
+   }
+}
+
+hexo.on('generateBefore', function(){
+   register_models(hexo);
+   DsBuilder(hexo);
+});
+
+hexo.extend.generator.register('clscontent', function(locals) {
+   return {
+      path: "/api",
+      layout: ["apidoc/index"],
+      data: {'xiux':1}
+   };
+});
